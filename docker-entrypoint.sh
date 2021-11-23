294fa8682e21b5ea8b5c7e2190e7800c9add9ded
makoto-kubodera <makoto-kubodera@linked-ideal.com>
2021-11-23 19:46:17 +0900

    https://github.com/toposoid/toposoid-sentence-parser-web/issues/8

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /app/toposoid-sentence-parser-web/target/universal
-toposoid-sentence-parser-web-0.1.1/bin/toposoid-sentence-parser-web
+toposoid-sentence-parser-web-0.1-SNAPSHOT/bin/toposoid-sentence-parser-web
